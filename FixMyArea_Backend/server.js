610c0961a28cbdc444b3c669b7ee9d4304e5d03c
Yogesh Jangid <yogeshjangid.it25@gmail.com>
2026-01-28 15:37:07 +0530

    Fix MongoDB connection env variables

--- FixMyArea_Backend/server.js
+++ FixMyArea_Backend/server.js
@@ -1,22 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// const dotenv = require('dotenv');
-
-// dotenv.config();
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.get('/api', (req, res) => {
-//   res.json({ message: 'Welcome to the Node.js Backend!' });
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
 
 
 import express from 'express';
